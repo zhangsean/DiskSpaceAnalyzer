dd54a6a83705085c80d43542b187d8ad578fb989
Zhangxf <zxf@aek56.com>
2017-09-01 09:05:20 +0800

    修复系统右键快捷分析无效

--- DiskSpaceAnalyzer/Program.cs
+++ DiskSpaceAnalyzer/Program.cs
@@ -9,11 +9,11 @@ namespace DiskSpaceAnalyzer
         /// 应用程序的主入口点。
         /// </summary>
         [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FormMain(new string[] { }));
+            Application.Run(new FormMain(args));
         }
     }
 }
